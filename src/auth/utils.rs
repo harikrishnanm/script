a40e1512ac2af11908bb04f7e9c01d66fd77d8e9
Harikrishnan M <hari@dmartlabs.com>
2021-09-18 12:11:12 +0530

    Cleaned up rbac code

--- src/auth/utils.rs
+++ src/auth/utils.rs
@@ -113,39 +113,38 @@ fn check_policy(rbac_params: &RbacParams, rbac: &Rbac, matches: &Vec<usize>) -> 
   match matches.len() {
     0 => false,
     _ => {
-      let (mut method_pass, mut user_pass, mut role_pass, mut pass ) = (false, false, false, false);
+      let mut pass = false ;
       for m in matches {
         debug!("Checking {} out of {:?}", m, matches);
         let methods_vec = methods.get(&m).unwrap();
-        method_pass = methods_vec.contains(&wildcard) || methods_vec.contains(&rbac_params.method);
+        pass = methods_vec.contains(&wildcard) || methods_vec.contains(&rbac_params.method);
         debug!(
-          "Checking for method match of {} in {:?}: {}",
+          "Checking for method match of {} in {:?} completed with {}",
           &rbac_params.method,
           methods.get(&m),
-          method_pass
+          pass
         );
         let users_vec = users.get(&m).unwrap();
-        user_pass = users_vec.contains(&wildcard) || users_vec.contains(&rbac_params.rbac_user);
+        pass = pass && (users_vec.contains(&wildcard) || users_vec.contains(&rbac_params.rbac_user));
         debug!(
-          "Checking for user match of {} in {:?}: {}",
+          "Checking for user match of {} in {:?} completed with {}",
           &rbac_params.rbac_user,
           users.get(&m),
-          user_pass
+          pass
         );
 
         let roles_vec = roles.get(&m).unwrap();
-        role_pass = roles_vec.contains(&wildcard);
-        if !role_pass {
+        if ! (pass && roles_vec.contains(&wildcard)) {
           for role in roles_vec {
-            if rbac_params.rbac_role.contains(role) {
-              role_pass = true;
+            if !rbac_params.rbac_role.contains(role) {
+              pass = false;
               break;
             }
           }
         }
-        if role_pass && method_pass && user_pass {break;}
+        if pass {break;}
       }
-      role_pass && method_pass && user_pass
+      pass
     }
   }
 }

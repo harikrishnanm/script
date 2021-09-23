9f5f8c9705c50c53f3326c0ed82d1c3f80a315af
Harikrishnan M <harikrishnanm@gmail.com>
2021-09-23 21:14:46 +0530

    Fixing module visiblilty

--- src/site/models.rs
+++ src/site/models.rs
@@ -13,13 +13,13 @@ pub struct NewSite {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Site {
-  site_id: Uuid,
-  name: String,
-  path: String,
-  slug: Option<String>,
-  url: Option<String>,
-  cors_enabled: Option<bool>,
-  created_by: String,
-  created: NaiveDateTime,
-  modified: NaiveDateTime,
+  pub site_id: Uuid,
+  pub name: String,
+  pub path: String,
+  pub slug: Option<String>,
+  pub url: Option<String>,
+  pub cors_enabled: Option<bool>,
+  pub created_by: String,
+  pub created: NaiveDateTime,
+  pub modified: NaiveDateTime,
 }
